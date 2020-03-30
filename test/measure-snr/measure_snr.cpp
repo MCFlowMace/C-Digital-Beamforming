933107c086de1f66a3e4c18096db6f75bd58bd04
Florian Thomas <flthomas@students.uni-mainz.de>
2020-03-30 18:24:55 +0200

    Add plot of SNR measurement

--- test/measure-snr/measure_snr.cpp
+++ test/measure-snr/measure_snr.cpp
@@ -33,11 +33,24 @@
 #include "reconstruction.hpp"
 #include "hpc_helpers.hpp"
 
+float calc_snr(float A, float N)
+{
+    return A*A/(N*N);
+}
+
+float calc_snr_err(float A, float N, float dA, float dN)
+{
+    float N2 = N*N;
+    float val1 = dA*2*A/N2;
+    float val2 = dN*2*A*A/(N2*N);
+    return sqrt(val1*val1 + val2*val2);
+}
+
 int main(int argc, char **argv)
 {
 
-    if(argc!=2) {
-        std::cerr << "args: [grid_size] !" << std::endl;
+    if(argc!=6) {
+        std::cerr << "args: [grid_size] [n_min] [n_max] [N_min] [N_max] !" << std::endl;
         exit(0);
     }
 
@@ -51,53 +64,79 @@ int main(int argc, char **argv)
     float R = 5.0f;
     int grid_size = std::atoi(argv[1]);
 
-    int n_min=100;
-    int n_max=1000;
+    int n_min=std::atoi(argv[2]);
+    int n_max=std::atoi(argv[3]);
     int n_skip=100;
 
-    for(int n_samples=n_min; n_samples<n_max; n_samples+=n_skip) {
+    int n_repeat = 50;
 
-        auto emission = [](float t, float w, float phi) -> float {
-                            float dw = 300*1e6;
-                            return cosf((w+dw*t)*t+phi);
-                        };
+    int N_min=std::atoi(argv[4]);
+    int N_skip=5;
+    int N_max = std::atoi(argv[5]);
 
-        Electron<float> e {w0, e_r*cosf(e_phi), e_r*sinf(e_phi), emission};
+    for(int N=N_min; N<N_max; N+= N_skip) {
+        for(int n_samples=n_min; n_samples<n_max; n_samples+=n_skip) {
+
+            arma::Col<float> amp_max (n_repeat);
+            arma::Col<float> amp_noise (n_repeat);
+
+            for(int i=0; i<n_repeat; ++i) {
+
+                std::cerr << N << " " << n_samples << std::endl;
+
+                auto emission = [](float t, float w, float phi) -> float {
+                                    float dw = 300*1e6;
+                                    return cosf((w+dw*t)*t+phi);
+                                };
+
+                Electron<float> e {w0, e_r*cosf(e_phi), e_r*sinf(e_phi), emission};
 
 
-        Antenna_Array<float> array(N, R, snr, wmix, sample_rate);
+                Antenna_Array<float> array(N, R, snr, wmix, sample_rate);
 
-        std::vector<Sample<float>> data;
+                std::vector<Sample<float>> data;
 
 
-        for(int i=0; i<N; ++i) {
-            data.push_back(array.antennas[i].sample_data(n_samples, 0.0f, e));
+                for(int i=0; i<N; ++i) {
+                    data.push_back(array.antennas[i].sample_data(n_samples, 0.0f, e));
+                }
+
+                Reconstruction<float> rec(grid_size, data[0].frequency, array);
+
+                //rec.set_antenna_array(array);
+
+                rec.run(data);
+
+                unsigned int index_max = rec.get_max_bin();
+
+                amp_max[i] = rec.get_max_val(index_max);
+
+                Electron<float> e2 {w0, e_r*cosf(e_phi), e_r*sinf(e_phi), [](float t, float w, float phi) -> float {return 0; }};
+
+                data.clear();
+
+                for(int i=0; i<N; ++i) {
+                    data.push_back(array.antennas[i].sample_data(n_samples, 0.0f, e2));
+                }
+
+                rec.run(data);
+
+                amp_noise[i] = rec.get_mean_val(index_max);
+
+                //std::cout << n_samples << " " << snr << std::endl;
+            }
+
+            float signal_mean = arma::mean(amp_max);
+            float noise_mean = arma::mean(amp_noise);
+
+            float signal_err = arma::stddev(amp_max)/sqrt(n_repeat);
+            float noise_err = arma::stddev(amp_noise)/sqrt(n_repeat);
+
+            float snr =calc_snr(signal_mean, noise_mean);
+            float dsnr = calc_snr_err(signal_mean, noise_mean, signal_err, noise_err);
+
+            std::cout << N << " " << n_samples << " " << snr << " " << signal_err << " " << noise_err << " " << dsnr << std::endl;
         }
-
-        Reconstruction<float> rec(grid_size, data[0].frequency, array);
-
-        //rec.set_antenna_array(array);
-
-        rec.run(data);
-
-        unsigned int index_max = rec.get_max_bin();
-
-        float amp_max = rec.get_max_val(index_max);
-
-        Electron<float> e2 {w0, e_r*cosf(e_phi), e_r*sinf(e_phi), [](float t, float w, float phi) -> float {return 0; }};
-
-        data.clear();
-
-        for(int i=0; i<N; ++i) {
-            data.push_back(array.antennas[i].sample_data(n_samples, 0.0f, e2));
-        }
-
-        rec.run(data);
-
-        float amp_noise = rec.get_mean_val(index_max);
-        float snr = amp_max*amp_max/(amp_noise*amp_noise);
-
-        std::cout << n_samples << " " << snr << std::endl;
     }
 }
 
