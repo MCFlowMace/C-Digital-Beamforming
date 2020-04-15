cda055e0b5f751f156827e164a220d5a121de8da
Florian Thomas <flthomas@students.uni-mainz.de>
2020-04-15 16:26:24 +0200

    Modify simulation class
    Fixes bug in the number of samples that are simulated. Also adds openmp
    parallelization of the data generation.

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -54,9 +54,9 @@ std::vector<std::vector<Sample<value_t>>> Simulation<value_t>::observation(
     Antenna_Array<value_t> array(settings.N, settings.R, settings.snr,
                                 settings.w_mix, settings.sample_rate);
 
-    std::vector<std::vector<Sample<value_t>>> data;
+    std::vector<std::vector<Sample<value_t>>> data(settings.N);
 
-    std::cout << "sample_rate: " << settings.sample_rate << " t_end " << t_end << std::endl;
+    //std::cout << "sample_rate: " << settings.sample_rate << " t_end " << t_end << std::endl;
 
     value_t dt = 1/settings.sample_rate;
     value_t delta_t = t_end-t_start;
@@ -64,19 +64,34 @@ std::vector<std::vector<Sample<value_t>>> Simulation<value_t>::observation(
     int n_packets = samples/settings.n_samples; //only take full packets of data
     //SDIV(samples, settings.n_samples);
 
-    std::cout << "samples: " << samples << " n_packets: " << n_packets << std::endl;
+    //std::cerr << "delta_t " << delta_t << " run time " << settings.run_duration << std::endl;
+    //std::cerr << "samples: " << samples << " n_packets: " << n_packets << std::endl;
 
+#ifdef PARALLEL
+    #pragma omp parallel
+    {
+        //std::cout << "threads: " << omp_get_num_threads() << std::endl;
+    #pragma omp for
+#endif
     for(int i=0; i<settings.N; ++i) {
+        //std::cout << i << " " << omp_get_thread_num() << std::endl;
         value_t t {t_start};
         //std::vector<Sample<value_t>> data_i(n_packets);
         std::vector<Sample<value_t>> data_i;
         for(int j=0; j<n_packets; ++j) {
             //data_i[j] = array.antennas[i].sample_data(samples, t, this->events);
-            data_i.push_back(array.antennas[i].sample_data(samples, t, this->events));
+            data_i.push_back(array.antennas[i].sample_data(settings.n_samples, t, this->events));
             t+=dt*samples;
+
+            //std::cout << "i, j " << i << ", " << j << std::endl;
         }
-        data.push_back(data_i);
+        //data.push_back(data_i);
+        data[i] = data_i;
     }
+
+#ifdef PARALLEL
+    }
+#endif
 
     return data;
 }
